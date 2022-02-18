b7b7bd8a62505da423235ead3a3d3f2895621845
hoangminhas <hoangminh9973@gmail.com>
2022-02-18 16:26:47 +0700

    last update

--- App/View/Food-Restaurant/food-list.php
+++ App/View/Food-Restaurant/food-list.php
@@ -11,7 +11,7 @@
     <?php foreach ($foods as $food):?>
     <tr>
         <td><?php echo $food->name?></td>
-        <td><img width="150px" style="padding: 20px" src="<?php echo $food->image?>" alt=""></td>
+        <td><img width="150px" style="padding: 20px" src="<?php echo $food->image?>" alt="nice"></td>
         <td><a onclick="return confirm('Are you sure ?')" href="index.php?page=food-delete&id=<?php echo $food->id?>">Delete</a></td>
         <td><a href="index.php?page=food-update&id=<?php echo $food->id?>">Update</a></td>
     </tr>
--- App/View/Food-Restaurant/food-update.php
+++ App/View/Food-Restaurant/food-update.php
@@ -1,3 +1,6 @@
+<div class="nav-bar">
+    <?php include "App/View/layout/navbar.php"?>
+</div>
 <form action=""method="post" enctype="multipart/form-data">
     FoodName <input type="text" name="name" value="<?php echo $foods->name?>"><br>
     <input type="file" name="image"><br>
