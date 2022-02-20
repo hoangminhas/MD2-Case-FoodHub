84d9feb1d85eefb1b9e278cbe4a6bf78752ad33a
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-02-20 20:39:58 +0700

    update

--- App/View/Food-Customer/cm-food-list.php
+++ App/View/Food-Customer/cm-food-list.php
@@ -1,25 +1,49 @@
-<div class="nav-bar">
-    <?php include "App/View/layout/navbar.php"?>
-</div>
-<table border="3px">
-    <thead>
-    <tr>
-        <th>Name</th>
-        <th>Phone</th>
-        <th>Email</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($restaurants as $restaurant):?>
-    <tr>
-        <td><?php echo $restaurant->name?></td>
-        <td><?php echo $restaurant->phone?></td>
-        <td><?php echo $restaurant->email?></td>
-        <td><a href="index.php?page=food-list<?php echo $restaurant->id?>">Menu</a></td>
+<!--<div class="nav-bar">-->
+<!--    --><?php //include "App/View/layout/navbar.php"?>
+<!--</div>-->
+<!--<table border="3px">-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <th>Name</th>-->
+<!--        <th>Phone</th>-->
+<!--        <th>Email</th>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($restaurants as $restaurant):?>
+<!--    <tr>-->
+<!--        <td>--><?php //echo $restaurant->name?><!--</td>-->
+<!--        <td>--><?php //echo $restaurant->phone?><!--</td>-->
+<!--        <td>--><?php //echo $restaurant->email?><!--</td>-->
+<!--        <td><a href="index.php?page=food-list--><?php //echo $restaurant->id?><!--">Menu</a></td>-->
+<!---->
+<!--    </tr>-->
+<!--    --><?php //endforeach;?>
+<!--    </tbody>-->
+<!--</table>-->
 
-    </tr>
-    <?php endforeach;?>
-    </tbody>
-</table>
 
+<?php include "App/View/layout/navbar.php" ?>
+<div class="container">
+    <h2 style="text-align: center">Menu</h2>
+    <table class="table" border="3px" style="margin: auto; text-align: center">
+        <thead STYLE="background-color: #7e9ed7">
+        <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($restaurants as $restaurant):?>
+            <tr>
+                <td><?php echo $restaurant->name?></td>
+                <td><?php echo $restaurant->phone?></td>
+                <td><?php echo $restaurant->email?></td>
+                <td><a href="index.php?page=food-list<?php echo $restaurant->id?>">Menu</a></td>
 
+            </tr>
+        <?php endforeach;?>
+        </tbody>
+    </table>
